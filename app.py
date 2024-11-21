53c034e50f16be1f47dc6c60199339d0ed79f898
jjcanot <jjcanot@eafit.edu.co>
2024-11-21 07:52:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def on_message(client, userdata, message):
     message_received=str(message.payload.decode("utf-8"))
     st.write(message_received)
 
-broker="broker.mqttdashboard.com"
+broker="157.230.214.127"
 port=1883
 client1= paho.Client("Canos")
 client1.on_message = on_message
